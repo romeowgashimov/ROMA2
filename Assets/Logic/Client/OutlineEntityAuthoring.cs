cddf47a6d1808ef66206d124fe3632d799e82e4f
romeowgashimov <xdarovaniye@gmail.com>
2026-05-04 21:32:42 +0700

    ещё

---
+++ Assets/Logic/Client/OutlineEntityAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Logic.Client
+{
+    public class OutlineEntityAuthoring : MonoBehaviour
+    {
+        private class OutlineEntityBaker : Baker<OutlineEntityAuthoring>
+        {
+            public override void Bake(OutlineEntityAuthoring authoring)
+            {
+                Entity entity = GetEntity(TransformUsageFlags.Renderable);
+                AddComponent<OutlineWidth>(entity);
+                AddComponent<OutlineColor>(entity);
+            }
+
+        }
+    }
+}
---
+++ Assets/Logic/Client/OutlineParentAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Logic.Client
+{
+    public class OutlineParentAuthoring : MonoBehaviour
+    {
+        public GameObject OutlinePrefab;
+        
+        private class OutlineBaker : Baker<OutlineParentAuthoring>
+        {
+            public override void Bake(OutlineParentAuthoring parentAuthoring)
+            {
+                Entity entity = GetEntity(TransformUsageFlags.Renderable);
+                AddComponent<OutlineEntityContainer>(entity, new()
+                {
+                    Value = GetEntity(parentAuthoring.OutlinePrefab, TransformUsageFlags.Renderable)
+                });
+            }
+        }
+    }
+}
